bda0130101124e71d134b047d6c1fa61808c2405
matt1003 <matt1003@gmail.com>
2025-04-04 22:48:00 +1300

    set git-signs stages icons/colors

--- nvim/lua/plugins/overrides/gitsigns.lua
+++ nvim/lua/plugins/overrides/gitsigns.lua
@@ -16,16 +16,32 @@ return {
     opts.signs.topdelete = { text = "△" }
     opts.signs.changedelete = { text = "┇" }
     opts.signs.untracked = { text = "┇" }
+    -- set staged symbols:
+    opts.signs_staged = opts.signs_staged or {}
+    opts.signs_staged.add = { text = "┃" }
+    opts.signs_staged.change = { text = "┃" }
+    opts.signs_staged.delete = { text = "▼" }
+    opts.signs_staged.topdelete = { text = "▲" }
+    opts.signs_staged.changedelete = { text = "┃" }
+    opts.signs_staged.untracked = { text = "┃" }
   end,
   config = function(_, opts)
     require("gitsigns").setup(opts)
     -- set highlighting:
-    vim.api.nvim_set_hl(0, "GitSignsAdd", { link = "GruvBoxGreen" })
-    vim.api.nvim_set_hl(0, "GitSignsChange", { link = "GruvBoxOrange" })
-    vim.api.nvim_set_hl(0, "GitSignsDelete", { link = "GruvBoxRed" })
-    vim.api.nvim_set_hl(0, "GitSignsTopdelete", { link = "GruvBoxRed" })
-    vim.api.nvim_set_hl(0, "GitSignsChangedelete", { link = "GruvBoxRed" })
+    vim.api.nvim_set_hl(0, "GitSignsAdd", { link = "GruvboxGreen" })
+    vim.api.nvim_set_hl(0, "GitSignsChange", { link = "GruvboxOrange" })
+    vim.api.nvim_set_hl(0, "GitSignsDelete", { link = "GruvboxRed" })
+    vim.api.nvim_set_hl(0, "GitSignsTopdelete", { link = "GruvboxRed" })
+    vim.api.nvim_set_hl(0, "GitSignsChangedelete", { link = "GruvboxRed" })
     vim.api.nvim_set_hl(0, "GitSignsUntracked", { link = "GruvboxPurple" })
+    -- set staged highlighting:
+    vim.api.nvim_set_hl(0, "GitSignsStagedAdd", { link = "GruvboxGreen" })
+    vim.api.nvim_set_hl(0, "GitSignsStagedChange", { link = "GruvboxOrange" })
+    vim.api.nvim_set_hl(0, "GitSignsStagedDelete", { link = "GruvboxRed" })
+    vim.api.nvim_set_hl(0, "GitSignsStagedTopdelete", { link = "GruvboxRed" })
+    vim.api.nvim_set_hl(0, "GitSignsStagedChangedelete", { link = "GruvboxRed" })
+    vim.api.nvim_set_hl(0, "GitSignsStagedUntracked", { link = "GruvboxPurple" })
+    -- set blame line highlighting:
     vim.api.nvim_set_hl(0, "GitSignsCurrentLineBlame", { link = "GruvboxBg3" })
     -- set key mappings:
     vim.api.nvim_set_keymap("n", "<c-n>", "<CMD>Gitsigns next_hunk<CR>", { silent = true })
