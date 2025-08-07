48f5dbe5733f7c9121dc319fbf6352277499d942
matt1003 <matt1003@gmail.com>
2025-08-07 21:20:23 +1200

    nvim: add muted colors for diagonstics

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,11 @@
 -- configure colorscheme
+
+-- muted:
+local DiagnosticMutedError = "#8f423d"
+local DiagnosticMutedWarn = "#99732e"
+local DiagnosticMutedInfo = "#556462"
+local DiagnosticMutedHint = "#56735f"
+
 return {
   {
     "ellisonleao/gruvbox.nvim",
